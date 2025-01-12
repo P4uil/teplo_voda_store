10aa07933c670a3581e487dab677657d3f082ddc
Totskiy Pavel <tockiypav@icloud.com>
2025-01-12 21:47:11 +0500

    nice working sewerage page

--- lib/pages/sewer/sewer_view.dart
+++ lib/pages/sewer/sewer_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:teplo_voda_store/components/tabbar/bloc/tabbar_bloc.dart';
 
 class SewerView extends StatelessWidget {
   const SewerView({super.key});
@@ -11,6 +13,39 @@ class SewerView extends StatelessWidget {
       ),
       body: Center(
         child: const Text('This is the Канализация page'),
+      ),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.grey[200],
+        ),
+        child: BlocBuilder<TabBarBloc, TabBarState>(
+          builder: (context, state) {
+            return BottomNavigationBar(
+              selectedItemColor: Colors.blue,
+              unselectedItemColor: Colors.grey,
+              currentIndex: state.selectedIndex,
+              type: BottomNavigationBarType.fixed,
+              onTap: (index) {
+                context.read<TabBarBloc>().add(SwitchTabEvent(index));
+                Navigator.pop(context); // Navigate back to the main tab view
+              },
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Магазин',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.shopping_cart),
+                  label: 'Корзина',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: 'Профиль',
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
